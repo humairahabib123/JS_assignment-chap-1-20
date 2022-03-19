c4bf8d2fdac4c0959f6deb5ab2cb8e9956e4d52f
humaira habib <drhumairahabib12@gmail.com>
2022-03-20 00:33:18 +0500

    Learning Java

--- chap 14-16/index.js
+++ chap 14-16/index.js
@@ -18,5 +18,47 @@ var mixArr = ["sadiqa", 9, "humaira", 7, "Tazmeen", 5, "Yuman", true, "Hoor"]
 
 // Question # 07:
 var qual = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
-var list = qual.aslist(arr);
-document.write(`${list}`)
+document.write(`${qual}<br/>`)
+
+// Question # 08:
+var names = ["Michael","John","Tony"];
+var marks = [320, 230, 480];
+
+// // Question # 09:
+// var colors = ["Yellow", "Brown", "White"];
+// document.write(`${colors} <br/>`);
+// var begColor = prompt(" what color do you want to add to the beginning of the array?\n");
+// document.write(`Adding ${begColor} at the biginning<br/>`)
+// var updated = colors.unshift(begColor);
+// document.write(`${updated} <br/>`)
+// var endColor = prompt(" what color do you want to add to the end of the array?\n");
+// document.write(`Adding ${endColor} at the biginning<br/>`)
+// var updatedEnd = colors.unshift(endColor);
+// document.write(`${updatedEnd} <br/>`);
+// var newColor = updatedEnd.unshift("mergenta", "Orange");
+// document.write(`${newColor} <br/>`);
+
+// Question # 10:
+var scores = [320, 230, 480, 120]
+var ordered = scores.sort();
+document.write(`Scores of Students: ${scores}<br/>`)
+document.write(`Ordered Scores of students: ${ordered}<br/>`);
+
+// Question # 11:
+var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+var selected = cities.slice(2, 4);
+document.write(`Cities List: ${cities}<br/>`)
+document.write(`Selected Cities List: ${selected}<br/>`);
+
+// Question # 12:
+var arr = ["This", "is", "my", "cat"];
+var joint = arr.join(' ');
+document.write(`Array: ${arr}<br/>`)
+document.write(`String: ${joint}<br/>`);
+
+// // Question # 13:
+// var devices = ["Keyboard", "mouse", "printer", "monitor"];
+
+// Question # 14:
+
+
